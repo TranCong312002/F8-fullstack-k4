fca09d3e91463a4e91de7d27d7d820cbe8b27dca
Trần Công <121604645+TranCong312002@users.noreply.github.com>
2023-08-15 20:16:07 +0700

    Bai tap

--- Bai_tap_buoi_18/javascript/Bai_tap.js
+++ Bai_tap_buoi_18/javascript/Bai_tap.js
@@ -2,22 +2,19 @@ var result = "";
 var editContent = `Lorem Ipsum is simply. dummy text of a b c d e f g `;
 var positions = editContent.indexOf(" ");
 var positionsStart = 0;
-console.log(positions);
+
 var change = function () {
-  console.log(editContent.length);
   result =
     editContent.slice(0, positionsStart) +
     `<span>${editContent.slice(positionsStart, positions)}</span>` +
     editContent.slice(positions);
-  console.log(result);
   positionsStart = positions + 1;
   positions = editContent.indexOf(" ", positions + 1);
-  console.log(positions);
   document.querySelector(".box").innerHTML = result;
   if (positions === -1) {
     positionsStart = 0;
     positions = editContent.indexOf(" ");
   }
 };
+setTimeout(change, 0);
 setInterval(change, 1000);
-console.log(change());
