9d634565587deb9cd750b82dd62ad6d9a9de034b
Trần Công <121604645+TranCong312002@users.noreply.github.com>
2023-08-15 18:04:47 +0700

    Bai tap

--- Bai_tap_buoi_18/javascript/Bai_tap.js
+++ Bai_tap_buoi_18/javascript/Bai_tap.js
@@ -1,27 +1,23 @@
 var result = "";
-var content = `Lorem Ipsum is simply. dummy text of `;
-var editContent = content;
-var positions = content.indexOf(" ");
+var editContent = `Lorem Ipsum is simply. dummy text of `;
+var positions = editContent.indexOf(" ");
 var positionsStart = 0;
 console.log(positions);
 var change = function () {
-  if (positions !== -1) {
-    console.log(editContent.length);
-    result =
-      editContent.slice(0, positionsStart) +
-      `<span>${editContent.slice(positionsStart, positions)}</span>` +
-      editContent.slice(positions);
-    console.log(result);
-    positionsStart = positions + 1;
-    positions = editContent.indexOf(" ", positions + 1);
-    console.log(positions);
-
-    result = content;
-    document.write(result);
-  } else {
+  console.log(editContent.length);
+  result =
+    editContent.slice(0, positionsStart) +
+    `<span>${editContent.slice(positionsStart, positions)}</span>` +
+    editContent.slice(positions);
+  console.log(result);
+  positionsStart = positions + 1;
+  positions = editContent.indexOf(" ", positions + 1);
+  console.log(positions);
+  document.querySelector(".box").innerHTML = result;
+  if (positions === -1) {
     positionsStart = 0;
     positions = editContent.indexOf(" ");
   }
 };
+setInterval(change, 1000);
 console.log(change());
-setInterval(change, 2000);
