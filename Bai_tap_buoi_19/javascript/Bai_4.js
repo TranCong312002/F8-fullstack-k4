e7ed54a4efb96cb166eda8dad820946d81392d03
Trần Công <121604645+TranCong312002@users.noreply.github.com>
2023-08-19 15:50:13 +0700

    Bài tập

--- Bai_tap_buoi_19/javascript/Bai_4.js
+++ Bai_tap_buoi_19/javascript/Bai_4.js
@@ -1,5 +1,6 @@
-var arr = [1, 7, 3, 8, 34, 6, 25, 100, 8];
-var add = 30;
+var arr = [5, 1, 9, 8, 10];
+console.log(`Dãy ban đầu: ${arr}`);
+var add = 4;
 if (Number.isInteger(add) === true) {
   if (Array.isArray(arr)) {
     var check = true;
@@ -12,14 +13,12 @@ if (Number.isInteger(add) === true) {
       arr.sort(function (a, b) {
         if (a < b) return -1;
       });
-      console.log(arr);
       for (var i = 1; i < arr.length; i++) {
-        console.log(add);
         if (arr[i - 1] < add && add < arr[i]) {
           arr.splice(i - 1, 2, arr[i - 1], add, arr[i]);
         }
       }
-      console.log(arr);
+      console.log(`Dãy sau khi add: ${arr}`);
     } else {
       console.log("Không phải dãy số nguyên. mời bạn nhập lại");
     }
