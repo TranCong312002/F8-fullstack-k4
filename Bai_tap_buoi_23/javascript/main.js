09d53271a57dc8c4e51164cf66e1c594a7202966
Trần Công <121604645+TranCong312002@users.noreply.github.com>
2023-09-10 11:57:31 +0700

    Bai tap

--- Bai_tap_buoi_23/javascript/main.js
+++ Bai_tap_buoi_23/javascript/main.js
@@ -1,6 +1,11 @@
 var login = document.querySelector("#login");
 var modal = document.querySelector(".modal");
 var modalOverlay = document.querySelector(".modal-overlay");
+var modalLogin = document.querySelector(".modal-login");
+var modalRegister = document.querySelector(".modal-register");
+var modalContentLogin = document.querySelector(".modal-content-login");
+var modalContentRegister = document.querySelector(".modal-content-register");
+var formRegister = document.querySelector(".form-register");
 console.log(login, modal);
 login.addEventListener("click", function () {
   if (!modal.classList.contains("js-modal")) {
@@ -12,4 +17,17 @@ login.addEventListener("click", function () {
 
 modalOverlay.addEventListener("click", function () {
   modal.classList.remove("js-modal");
+});
+
+modalLogin.addEventListener("click", function () {
+  modalLogin.classList.add("active");
+  modalRegister.classList.remove("active");
+  modalContentLogin.classList.remove("hidden");
+  modalContentRegister.classList.add("hidden");
+});
+modalRegister.addEventListener("click", function () {
+  modalRegister.classList.add("active");
+  modalContentLogin.classList.add("hidden");
+  modalContentRegister.classList.remove("hidden");
+  modalLogin.classList.remove("active");
 });
